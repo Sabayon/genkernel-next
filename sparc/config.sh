99521b2922ce009373059c80184cb7229897de55
Jason Wever <weeve@gentoo.org>
2004-01-05 01:53:54 +0000

    Added sparc and sparc64 dirs for genkernel.
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/genkernel/trunk@37 67a159dc-881f-0410-a524-ba9dfbe2cb84

---
+++ sparc/config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# sparc/config.sh
+
+KERNEL_MAKE_DIRECTIVE="vmlinux"
+KERNEL_MAKE_DIRECTIVE_2=""
+KERNEL_BINARY="vmlinux"
+
+# The dietlibc portion of busybox is commented out right now
+# other stuff seems to compile fine though
+USE_DIETLIBC=0
+
+KERNEL_MAKE=make
+UTILS_MAKE=make
+
+KERNEL_CC=gcc
+
+UTILS_CC=gcc
+UTILS_AS=as
+UTILS_LD=ld
+
+COMPRESS_INITRD=yes
+BOOTSPLASH="no"
+USECOLOR="no"
---
+++ sparc64/config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#sparc64-config.sh
+
+KERNEL_MAKE_DIRECTIVE="vmlinux"
+KERNEL_MAKE_DIRECTIVE_2="image"
+KERNEL_BINARY="arch/sparc64/boot/image"
+
+# Busybox 1.00-pre3 won't build with dietlibc, when it does we
+# can turn this flag on
+USE_DIETLIBC=0
+
+KERNEL_MAKE=make
+UTILS_MAKE=make
+
+KERNEL_CC=sparc64-linux-gcc
+#KERNEL_AS=as
+#KERNEL_LD=ld
+
+UTILS_CC=gcc
+UTILS_AS=as
+UTILS_LD=ld
+
+COMPRESS_INITRD=yes
+BOOTSPLASH="no"
+USECOLOR="no"
