d02b87e6984919acb8d1b33347f0cae823291816
Eric Edgar <rocket@gentoo.org>
2005-07-28 15:57:37 +0000

    Fix cp /dev/tty1 error message
    git-svn-id: svn+ssh://svn.gentoo.org/var/svnroot/genkernel/trunk@283 67a159dc-881f-0410-a524-ba9dfbe2cb84

--- gen_initrd.sh
+++ gen_initrd.sh
@@ -56,7 +56,7 @@ create_base_initrd_sys() {
 	cd ${TEMP}/initrd-temp/dev
 	MAKEDEV std
 	MAKEDEV console
-
+	
 	if [ "${DISKLABEL}" -eq '1' ]; then
 		cp "${BLKID_BINCACHE}" "${TEMP}/initrd-temp/bin/blkid.bz2" ||
 			gen_die 'Could not copy blkid from bincache!'
