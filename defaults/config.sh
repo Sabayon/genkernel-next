27407030ff2bbb253309726770444a60d2d032d0
Chris Gianelloni <wolf31o2@gentoo.org>
2008-07-28 14:48:30 -0700

    Added a defaults directory to store default files for builds.  These are overridden by architecture or module specific parameters.  This should end up reducing the amount of architecture-specific files needed, especially as most of them have files which are (nearly) identical.

---
+++ defaults/config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# Arch-specific options that normally shouldn't be changed.
+#
+KERNEL_MAKE_DIRECTIVE="bzImage"
+KERNEL_MAKE_DIRECTIVE_2=""
+KERNEL_BINARY="arch/i386/boot/bzImage"
+
+COMPRESS_INITRD=yes
+
+#
+# Arch-specific defaults that can be overridden in the config file or on the
+# command line.
+#
+DEFAULT_MAKEOPTS="-j2"
+
+DEFAULT_KERNEL_MAKE=make
+DEFAULT_UTILS_MAKE=make
+
+DEFAULT_KERNEL_CC=gcc
+DEFAULT_KERNEL_AS=as
+DEFAULT_KERNEL_LD=ld
+
+DEFAULT_UTILS_CC=gcc
+DEFAULT_UTILS_AS=as
+DEFAULT_UTILS_LD=ld
