e4f3b17b3afa6af5719c633ea7982de81996eebe
agaffney <agaffney@kagome.(none)>
2008-11-13 11:03:03 -0600

    Compile unionfs-fuse statically

--- gen_compile.sh
+++ gen_compile.sh
@@ -564,7 +564,7 @@ compile_unionfs_fuse() {
 		cd "${UNIONFS_FUSE_DIR}"
 		print_info 1 'unionfs-fuse: >> Compiling...'
 		sed -i "/^\(CFLAGS\|CPPFLAGS\)/s:^\\(.*\\)$:\\1 -I${TEMP}/${FUSE_DIR}/include -L${TEMP}/${FUSE_DIR}/lib/.libs:" Makefile src/Makefile
-		sed -i "/^LIB = /s:^LIB = \(.*\)$:LIB = -L${TEMP}/${FUSE_DIR}/lib/.libs \1:" Makefile src/Makefile
+		sed -i "/^LIB = /s:^LIB = \(.*\)$:LIB = -static -ldl -lrt -L${TEMP}/${FUSE_DIR}/lib/.libs \1:" Makefile src/Makefile
 		MAKE=${UTILS_MAKE} compile_generic "" ""
 		print_info 1 'unionfs-fuse: >> Copying to cache...'
 		[ -f "${TEMP}/${UNIONFS_FUSE_DIR}/src/unionfs" ] ||
