3d7de35394a704c993b9986c2e70adcd4083ee6b
Robin H. Johnson <robbat2@gentoo.org>
2011-07-26 22:34:00 +0000

    Bug #376467: fix kernel version checks for 3.0 kernel.
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- gen_arch.sh
+++ gen_arch.sh
@@ -51,7 +51,10 @@ set_kernel_arch() {
 	KERNEL_ARCH=${ARCH}
 	case ${ARCH} in
 		ppc|ppc64)
-			if [ "${VER}" -eq "2" -a "${PAT}" -ge "6" ]
+			if [ "${VER}" -ge "3" ]
+			then
+					KERNEL_ARCH=powerpc
+			elif [ "${VER}" -eq "2" -a "${PAT}" -ge "6" ]
 			then
 				if [ "${PAT}" -eq "6" -a "${SUB}" -ge "16" ] || [ "${PAT}" -gt "6" ]
 				then
@@ -60,7 +63,10 @@ set_kernel_arch() {
 			fi
 			;;
 		x86)
-			if [ "${VER}" -eq "2" -a "${PAT}" -ge "6" ] || [ "${VER}" -gt "2" ]
+			if [ "${VER}" -ge "3" ]
+			then
+					KERNEL_ARCH=x86
+			elif [ "${VER}" -eq "2" -a "${PAT}" -ge "6" ] || [ "${VER}" -gt "2" ]
 			then
 				if [ "${PAT}" -eq "6" -a "${SUB}" -ge "24" ] || [ "${PAT}" -gt "6" ]
 				then
@@ -71,7 +77,10 @@ set_kernel_arch() {
 			fi
 			;;
 		x86_64)
-			if [ "${VER}" -eq "2" -a "${PAT}" -ge "6" ] || [ "${VER}" -gt "2" ]
+			if [ "${VER}" -ge "3" ]
+			then
+					KERNEL_ARCH=x86
+			elif [ "${VER}" -eq "2" -a "${PAT}" -ge "6" ] || [ "${VER}" -gt "2" ]
 			then
 				if [ "${PAT}" -eq "6" -a "${SUB}" -ge "24" ] || [ "${PAT}" -gt "6" ]
 				then
